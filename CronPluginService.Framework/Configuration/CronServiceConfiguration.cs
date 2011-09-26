a7d5b224364e00331dcfa6f212bf886c391bcc61
Scott McCain <scott.mccain@gmail.com>
2011-09-26 08:19:07 -0700

    Added JobParameters to JobInfo node in configuration.

--- CronPluginService.Framework/Configuration/CronServiceConfiguration.cs
+++ CronPluginService.Framework/Configuration/CronServiceConfiguration.cs
@@ -8,26 +8,12 @@ namespace CronPluginService.Framework.Configuration
 {
     public class CronServiceConfiguration : ConfigurationSection
     {
-        //[ConfigurationProperty("", IsRequired = true, IsDefaultCollection = true)]
-        //public CronServiceInstanceCollection Instances
-        //{
-        //    get { return (CronServiceInstanceCollection)this[""]; }
-        //    set { this[""] = value; }
-        //}
-
         [ConfigurationProperty("Jobs", IsRequired = true)]
         public CronScheduledJobCollection Jobs
         {
             get { return (CronScheduledJobCollection)this["Jobs"]; }
             set { this["Jobs"] = value; }
         }
-
-        //[ConfigurationProperty("JobParameters", IsRequired = true)]
-        //public CronServiceJobParameterCollection JobParameters
-        //{
-        //    get { return (CronServiceJobParameterCollection)this["JobParameters"]; }
-        //    set { this["JobParameters"] = value; }
-        //}
 
         [ConfigurationProperty("PluginConfiguration", IsRequired = true)]
         public CronServicePluginInfoCollection PluginInfo
